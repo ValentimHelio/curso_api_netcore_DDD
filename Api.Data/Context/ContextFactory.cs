a75854f300874dee985cd8b66fecbdb0c788ca64
valentimhelio <valentimhelio@gmail.com>
2025-04-15 14:08:53 -0400

    57. Api.Data - Criar ContextFactory

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //Usado para criar as Migrações
+            var connectionString = "Server=localhost;DataBase=ApiCurso_DB;Uid=valentim;Pwd=123456";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
